486b559e557df3e948f006544faf7326a2f6c5e3
Nastya <annika-purtova@yandex.ru>
2022-11-24 22:21:39 +0300

    Solution for 1st exercise

--- 1_if1.py
+++ 1_if1.py
@@ -14,12 +14,21 @@
 
 """
 
-def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+user_age = int(input('Insert your age: '))
+
+def main(user_age):
+
+    if user_age < 3:
+      print('Relax')
+      
+    if user_age >= 3 and user_age < 7:
+      print('You should be in kindergarten!')
+    elif user_age >= 7 and user_age <= 17:
+      print('You should be in school!')
+    elif user_age > 17 and user_age <= 25:
+      print('You should be in university!')
+    elif user_age > 25:
+      print('Back to work!')
 
 if __name__ == "__main__":
-    main()
+    main(user_age)
